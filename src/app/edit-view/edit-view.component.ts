ffd2d51502d8ee1c732987e35296dbbe699ebb13
lustigepepe <lustigepepe@web.de>
2018-11-27 17:18:57 +0100

    new feedback

--- src/app/edit-view/edit-view.component.ts
+++ src/app/edit-view/edit-view.component.ts
@@ -80,7 +80,11 @@ export class EditComponent implements OnInit {
     let str: string = this.editForm.value.dateTime;    
     str = str.replace(/\./g, '/');
     const val = moment(str, "DD/MM/YYYY / HH:mm", true);
-    if (!val.isValid()) {
+    console.log(this.editForm.controls.euro.touched);
+    console.log(!val.isValid());
+    if (this.editForm.controls.dateTime.touched && !val.isValid()) {
+    console.log('!val.isValid()');
+      
       this.wrongDateTime = true;
       return;
     }
